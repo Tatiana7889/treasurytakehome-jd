bfaa6da183d54d2b60e04fc17cededa16d65a35f
Tatianatim <94038954+Tatianatim@users.noreply.github.com>
2026-09-15 18:31:24 -0400

    Initial commit

--- src/Ttb.LabelReview.Api/obj/Debug/net8.0/Ttb.LabelReview.Api.AssemblyInfo.cs
+++ src/Ttb.LabelReview.Api/obj/Debug/net8.0/Ttb.LabelReview.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ttb.LabelReview.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bae66d98a88d3a1131f33242d3958d8ef4f0e7d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f85ed9d08b7669c0315bcd9ce02dcfd6fceb77c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ttb.LabelReview.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ttb.LabelReview.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
